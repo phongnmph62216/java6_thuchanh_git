61f99e3287f9dafb8f3a8f5fe5dd77d888c9542f
fuga26 <minhphong26012006@gmail.com>
2026-04-24 01:03:05 +0700

    update regiter

---
+++ src/main/java/com/example/java6n_sp26_sd20305/util/regiter.java
@@ -0,0 +1,13 @@
+package com.example.java6n_sp26_sd20305.util;
+
+public class regiter {
+
+    public static void main(String[] args) {
+        System.out.printf("trang dang ky");
+        System.out.printf("trang dang ky");
+        System.out.printf("trang dang ky");
+        System.out.printf("trang dang ky");
+        System.out.printf("trang dang ky");
+        System.out.printf("trang dang ky");
+    }
+}
