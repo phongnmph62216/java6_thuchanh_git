4476782e56386d84e0046a5579fac69e80a5eb3a
fuga26 <minhphong26012006@gmail.com>
2026-04-23 23:10:17 +0700

    update paste contat

---
+++ src/main/java/com/example/java6n_sp26_sd20305/util/contat.java
@@ -0,0 +1,9 @@
+package com.example.java6n_sp26_sd20305.util;
+
+public class contat {
+
+    public static void main(String[] args) {
+        System.out.printf("thay doi tren nay  thya doi tren nafy 12312231");
+        System.out.printf("t234234242423423");
+    }
+}
